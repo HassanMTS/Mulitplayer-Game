87a3f9b71fffef9ef6930aba04aa39f0ffc9135a
HassanMTS <150341521+HassanMTS@users.noreply.github.com>
2024-02-02 16:48:42 +0000

    Changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
-const io = new Server(server);
+const io = new Server(server, { pingInterval: 200, pingTimeout: 5000})
 
 const port = 3000;
 
